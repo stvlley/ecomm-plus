09908f0261d748181780e61b47ed98703f71f8b2
Stephen <stvlley.amzn@gmail.com>
2022-06-27 13:00:01 -0400

    save for safety

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDzafGKJmSccTOI-Bxfxb_o4Gd95-Cr3K8",
+    authDomain: "ecomm-plus.firebaseapp.com",
+    projectId: "ecomm-plus",
+    storageBucket: "ecomm-plus.appspot.com",
+    messagingSenderId: "551745377875",
+    appId: "1:551745377875:web:19921e53ecfc766d2298a6",
+    measurementId: "G-CYDSRSN363"
+  };
